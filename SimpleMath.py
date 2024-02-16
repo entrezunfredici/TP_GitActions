56a3abd9b3378c6cc786318443344146c7953c50
entrezunfredici <superfred2468@gmail.com>
2024-02-16 15:13:50 +0100

    [ADD] simpleMath class

---
+++ SimpleMath.py
@@ -0,0 +1,12 @@
+import math
+
+class SimpleMath:
+    print("=======================================")
+    print("Welcome in SimpleMath class")
+    a=int(input("entrez a ⇒ "))
+    b=int(input("entrez b ⇒ "))
+    def add(a, b):
+        return a+b
+
+    print(add(a, b))
+    print("=======================================")
